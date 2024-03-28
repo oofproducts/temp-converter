276280bf05b048c492de026727d61a1dabe6b136
oofproducts <114316532+oofproducts@users.noreply.github.com>
2024-03-28 11:05:01 -0600

    might break

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 illegal_temperature = False
 
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+
 st.write('Weleedeeeeecome to Streamlit')
