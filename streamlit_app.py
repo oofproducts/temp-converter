4f64af5a15f3e34b3ce085fbbde2d737edbf8188
oofproducts <114316532+oofproducts@users.noreply.github.com>
2024-03-28 11:08:22 -0600

    Comversion unit added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,14 @@ import streamlit as st
 
 illegal_temperature = False
 
+temp = st.number_input('What is your temperature?')
+
 initial_unit = st.radio(
     "What is the starting unit of your temperature?",
+    ["Celsius", "Fahrenheit", "Kelvin"],)
+
+conversion_unit = st.radio(
+    "What unit would you like to convert to?",
     ["Celsius", "Fahrenheit", "Kelvin"],)
 
 st.write('Weleedeewxxxeeeecome to Streamlit')
