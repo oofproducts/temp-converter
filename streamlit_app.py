aba2bcc8cfa49d88108c9417ee193b0695ec084c
oofproducts <114316532+oofproducts@users.noreply.github.com>
2024-03-28 11:07:02 -0600

    Fix for initial unit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ illegal_temperature = False
 
 initial_unit = st.radio(
     "What is the starting unit of your temperature?",
-    ["Celsius", "Fahrenheit", "Kelvin"],
+    ["Celsius", "Fahrenheit", "Kelvin"],)
 
 st.write('Weleedeewxxxeeeecome to Streamlit')
